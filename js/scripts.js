f7051cab67f8afc0e1aeb97ae362fb76ad0c1158
nguyenquochung2357 <carzygaming2222@gmail.com>
2021-07-06 21:27:12 +0700

    third

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,3 @@
-/*!
-* Start Bootstrap - Business Frontpage v5.0.3 (https://startbootstrap.com/template/business-frontpage)
-* Copyright 2013-2021 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+AOS.init({
+    duration: 1200,
+  })
